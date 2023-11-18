408de093b403dda6ab1b0c7a1cd4ab1dae1b602b
yang_zhuo <2431542525@qq.com>
2023-11-18 16:07:37 +0800

    init

--- src/index.js
+++ src/index.js
@@ -4,6 +4,31 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { registerMicroApps, start } from 'qiankun';
+
+// 注册应用
+registerMicroApps([
+  {
+    name: 'qiankun-micro-app1', 
+    entry: '//localhost:3032',
+    container: '#micro-app1',
+    activeRule: '/micro-app1',
+  },
+  {
+    name: 'qiankun-micro-app2',
+    entry: '//localhost:3033',
+    container: '#micro-app2',
+    activeRule: '/micro-app2',
+    // 传值
+    props:{
+      nickname:"杨卓",
+      age:18
+    }
+  },
+]);
+
+start();
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
